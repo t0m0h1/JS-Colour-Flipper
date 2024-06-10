de12137ba0de4428eb031c8f0a28a4eda76f06c8
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-06-10 15:48:15 +0100

    Fix typo in variable name

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const colours = ["red", "green", "blue", "yellow", "pink", "purple", "orange", "brown", "black", "white"];
 const btn = document.getElementById("btn");
-const colour = document.querySelector(".color");
+const colour = document.querySelector(".colour");
 
 btn.addEventListener("click", function() {
   console.log(document.body);

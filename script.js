687b65030b255cbc2fbfb32eec487fb3c3175696
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-06-10 15:47:31 +0100

    Update button ID and fix typo in script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const colours = ["red", "green", "blue", "yellow", "pink", "purple", "orange", "brown", "black", "white"];
 const btn = document.getElementById("btn");
-const colour = document.querySelector(".colour");
+const colour = document.querySelector(".color");
 
 btn.addEventListener("click", function() {
+  console.log(document.body);
   const randomNumber = getRandomNumber();
   document.body.style.backgroundColor = colours[randomNumber];
   colour.textContent = colours[randomNumber];

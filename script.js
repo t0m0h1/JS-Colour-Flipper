4ffa6a82b6594db8d5f26afdae1f400d3a55cd7b
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-06-10 15:40:01 +0100

    Add button and colour element to script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 const colours = ["red", "green", "blue", "yellow", "pink", "purple", "orange", "brown", "black", "white"];
+const btn = document.getElementById("btn");
+const colour = document.querySelector(".colour");

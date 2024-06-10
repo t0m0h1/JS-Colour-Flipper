6a0abb900442bd0ee513d30c1d199e9bebcbf6cf
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-06-10 15:41:50 +0100

    Add event listener for button click and getRandomNumber function

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const colours = ["red", "green", "blue", "yellow", "pink", "purple", "orange", "brown", "black", "white"];
 const btn = document.getElementById("btn");
 const colour = document.querySelector(".colour");
+
+btn.addEventListener("click", function() {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colours[randomNumber];
+  colour.textContent = colours[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colours.length);
+}

965b05b55323b3f0a55b8ce740b1c87699f1f005
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-06-10 15:51:54 +0100

    Update script.js to add event listener for DOMContentLoaded

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const colours = ["red", "green", "blue", "yellow", "pink", "purple", "orange", "brown", "black", "white"];
-const btn = document.getElementById("btn");
-const colour = document.querySelector(".colour");
+document.addEventListener('DOMContentLoaded', (event) => {
+    const colours = ["red", "green", "blue", "yellow", "pink", "purple", "orange", "brown", "black", "white"];
+    const btn = document.getElementById("btn");
+    const colour = document.querySelector(".colour");
 
-btn.addEventListener("click", function() {
-  console.log(document.body);
-  const randomNumber = getRandomNumber();
-  document.body.style.backgroundColor = colours[randomNumber];
-  colour.textContent = colours[randomNumber];
+    btn.addEventListener("click", function() {
+        const randomNumber = getRandomNumber();
+        document.body.style.backgroundColor = colours[randomNumber];
+        colour.textContent = colours[randomNumber];
+    });
+
+    function getRandomNumber() {
+        return Math.floor(Math.random() * colours.length);
+    }
 });
-
-function getRandomNumber() {
-  return Math.floor(Math.random() * colours.length);
-}
